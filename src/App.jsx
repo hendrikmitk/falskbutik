1236edc3b050ed620c3e165323996963655f7923
hendrikmitk <hendrikharlichs@fastmail.com>
2022-02-17 19:42:28 +0100

    feat: Install axios; integrate API; display products

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from 'react';
+import api from './api/axios';
+
 function App() {
-  return <div className="App">Hello World</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    let data = await api.get('/products').then(({ data }) => data);
+    setProducts(data);
+  };
+
+  return (
+    <div className="App">
+      <ul>
+        {products.map((product, index) => {
+          return <li key={product.id}>{product.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
