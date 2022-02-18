7ab6eb2c34125b719ddd720c7e08e89f6480a15f
hendrikmitk <hendrikharlichs@fastmail.com>
2022-02-18 18:38:27 +0100

    feat: Introduce About, Home and Products page

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,18 @@
-import { useEffect, useState } from 'react';
-import api from './api/axios';
-import shuffle from './functions/shuffle';
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import ProductList from './components/ProductList';
+import Products from './pages/Products';
 
-function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-  const getProducts = async () => {
-    let data = await api.get('/products').then(({ data }) => data);
-    setProducts(shuffle(data));
-    setIsLoading(false);
-  };
-
+const App = () => {
   return (
     <div className="App font-spartan text-white">
       <Header />
       <div className="min-h-screen bg-blue-dark px-32 pt-28">
-        <h2 className="text-4xl font-medium uppercase">Our products</h2>
-        {isLoading ? (
-          <div className="flex items-center justify-center pt-32 text-2xl">
-            Loading ...
-          </div>
-        ) : (
-          <ProductList products={products} />
-        )}
+        <Products />
       </div>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Products.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import ProductList from '../components/ProductList';
+import api from '../api/axios';
+import shuffle from '../functions/shuffle';
+
+const Products = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    let data = await api.get('/products').then(({ data }) => data);
+    setProducts(shuffle(data));
+    setIsLoading(false);
+  };
+
+  return (
+    <div>
+      <h2 className="text-4xl font-medium uppercase">Our products</h2>
+      {isLoading ? (
+        <div className="flex items-center justify-center pt-32 text-2xl">
+          Loading ...
+        </div>
+      ) : (
+        <ProductList products={products} />
+      )}
+    </div>
+  );
+};
+
+export default Products;
