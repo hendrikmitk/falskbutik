01ecf0eac088c29212b96bd77f1933b17f35b707
hendrikmitk <hendrikharlichs@fastmail.com>
2022-02-19 17:39:36 +0100

    fix: Adjust footer properties slightly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-blue-light flex h-12 shrink-0 items-center justify-center">
       <div className="text-orange flex items-center space-x-2 font-mono text-xs">
         <p>Made with ❤️ in Hamburg</p>
-        <span className="-translate-y-[2px] text-xl">|</span>
+        <span className="text-lg">|</span>
         <p>
           Source available on{' '}
           <a href="https://github.com/hendrikmitk/falskbutik/">GitHub</a>
