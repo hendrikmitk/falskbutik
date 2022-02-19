7f8a54bffa61d7b3f36729317942a9e41551a57b
hendrikmitk <hendrikharlichs@fastmail.com>
2022-02-19 18:00:56 +0100

    feat: Implement product detail modal

--- src/components/ProductListItem.jsx
+++ src/components/ProductListItem.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import ProductListItemRating from './ProductListItemRating';
 import ProductListItemFavorite from './ProductListItemFavorite';
 import ProductListItemCategory from './ProductListItemCategory';
+import ProductListItemDetail from './ProductListItemDetail';
 
 const ProductListItem = ({ product }) => {
   const [isFavorite, setIsFavorite] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleFavoriteButtonHandler = () =>
     setIsFavorite((isFavorite) => !isFavorite);
+
+  const toggleModal = () => setIsModalOpen((isModalOpen) => !isModalOpen);
 
   return (
     <div className="bg-blue-light flex w-full flex-col space-y-2 rounded-lg px-4 py-6">
@@ -18,11 +22,21 @@ const ProductListItem = ({ product }) => {
           isFavorite={isFavorite}
         />
       </div>
-      <p className="text-lg font-medium">{product.title}</p>
+      <p onClick={toggleModal} className="cursor-pointer text-lg font-medium">
+        {product.title}
+      </p>
       <div className="flex justify-between">
         <p className="font-mono text-sm">{product.price} USD</p>
         <ProductListItemCategory category={product.category} />
       </div>
+      {isModalOpen && (
+        <ProductListItemDetail
+          isFavorite={isFavorite}
+          product={product}
+          toggleFavoriteButtonHandler={toggleFavoriteButtonHandler}
+          toggleModal={toggleModal}
+        />
+      )}
     </div>
   );
 };
---
+++ src/components/ProductListItemDetail.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import CloseIcon from './CloseIcon';
+import ProductListItemCategory from './ProductListItemCategory';
+import ProductListItemFavorite from './ProductListItemFavorite';
+import ProductListItemRating from './ProductListItemRating';
+
+const ProductListItemDetail = ({
+  isFavorite,
+  product,
+  toggleFavoriteButtonHandler,
+  toggleModal,
+}) => {
+  return (
+    <div className="bg-blue-dark/90 fixed inset-0 h-screen select-none overflow-y-hidden">
+      <div className="bg-blue-light mx-auto mt-40 flex w-2/3 select-text flex-col rounded-lg px-4 py-6">
+        <div
+          onClick={toggleModal}
+          className="text-orange flex cursor-pointer justify-end"
+        >
+          <div className="transition-all duration-200 hover:rotate-90">
+            <CloseIcon />
+          </div>
+        </div>
+        <div className="flex flex-col space-y-6 px-1 pb-6">
+          <ProductListItemRating rating={product.rating.rate} />
+          <p className="text-lg font-medium">{product.title}</p>
+          <p className="text-sm">{product.description}</p>
+          <div className="flex justify-between">
+            <p className="font-mono text-sm">{product.price} USD</p>
+            <ProductListItemCategory category={product.category} />
+            <ProductListItemFavorite
+              onClick={toggleFavoriteButtonHandler}
+              isFavorite={isFavorite}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductListItemDetail;
